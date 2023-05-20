20347db9841aaec317a117635db61503c299a36f
Alberto Centonze <albicento.ac@gmail.com>
2023-05-20 22:46:20 +0200

    Version compatiblity

--- src/SecretGuardiansModule.sol
+++ src/SecretGuardiansModule.sol
@@ -1,13 +1,14 @@
-/// SPDX-License-Identifier: MIT
-pragma solidity 0.8.16;
+// SPDX-License-Identifier: Unlicensed
+pragma solidity 0.8.17;
 
-import "zk-connect-onchain-verifier/src/libs/zk-connect/ZkConnectLib.sol";
+import "sismo-connect-packages/SismoLib.sol";
+import "./GnosisSafe.sol";
 
-contract zkConnectModule is ZkConnect {
+contract SecretGuardiansModule is SismoConnect {
     bytes16 public groupId;
     GnosisSafe public safe;
 
-    constructor(address _safe, bytes16 _appId, bytes16 _groupId) ZkConnect(_appId) {
+    constructor(address _safe, bytes16 _appId, bytes16 _groupId) SismoConnect(_appId) {
         safe = GnosisSafe(_safe);
         groupId = _groupId;
     }
@@ -19,29 +20,17 @@ contract zkConnectModule is ZkConnect {
         groupId = _groupId;
     }
 
-    /// @dev Exec tx using zkConnect proof
-    /// @param to Destination address of module transaction
-    /// @param value Ether value of module transaction
-    /// @param data Data payload of module transaction
-    /// @param operation Operation type of module transaction
-    function execTransactionFromModule(
-        address to,
-        uint256 value,
-        bytes calldata data,
-        Enum.Operation operation,
-        bytes memory zkConnectResponse
-    ) public virtual returns (bool success) {
-        //TODO: should use a nonce to avoid replay
-
-        ZkConnectVerifiedResult memory zkConnectVerifiedResult = verify({
-            responseBytes: zkConnectResponse,
-            authRequest: buildAuth({authType: AuthType.ANON}),
-            claimRequest: buildClaim({groupId: groupId}),
-            messageSignatureRequest: abi.encode(to, value, data, operation)
+    function _verifyRecoverProof(bytes memory proof) internal {
+        verify({
+            responseBytes: proof,
+            auth: buildAuth({authType: AuthType.VAULT}),
+            claim: buildClaim({groupId: groupId}),
+            signature: buildSignature({message: abi.encode(msg.sender)})
         });
+    }
 
-        require(safe.execTransactionFromModule(to, value, data, operation), "Module transaction failed");
-
-        return true;
+    function helpRecover(address newOwner, bytes memory proof) external {
+        _verifyRecoverProof(proof);
+        GnosisSafe(safe).addOwnerWithThreshold(newOwner, 1);
     }
 }
