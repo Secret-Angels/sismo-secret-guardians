fd6d9ce6b60bc81e22808ff3bb0d73ee32a09db6
Mouran Antoine <32228897+L0GYKAL@users.noreply.github.com>
2023-05-21 03:23:50 +0200

    Test edit

--- src/ISocialRecovery.sol
+++ src/ISocialRecovery.sol
@@ -3,11 +3,10 @@ pragma solidity 0.8.17;
 
 interface ISocialRecovery {
 
-    function initiateRecovery(bytes memory proof) external;
     // make it only onwer
     function denyRecover() external;
 
-    function supportRecover() external;
+    function supportRecover(bytes memory proof) external;
 
     function executeRecovery() external;
 
